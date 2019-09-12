310f2a9458b31621fa45145117fc10ae2ec80c0c
lerry <xanthouson@126.com>
2019-09-13 02:06:27 +0800

    Initial commit

---
+++ ConsumeAPI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsumeAPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
